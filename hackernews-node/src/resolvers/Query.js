91a81ccdb49fadd9a77a0260685e297770a54caf
Jiggins242 <44429273+Jiggins242@users.noreply.github.com>
2018-12-14 09:51:26 +0000

    feed feature can now count

--- hackernews-node/src/resolvers/Query.js
+++ hackernews-node/src/resolvers/Query.js
@@ -1,6 +1,6 @@
 
 
-function feed(parent, args, context, info) {
+async function feed(parent, args, context, info) {
     const where = args.filter
     ? {
         OR: [
@@ -10,10 +10,24 @@ function feed(parent, args, context, info) {
     }
     : {}
 
-    return context.db.query.links(
+    const queriedLinks = await context.db.query.links(
         { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
-         info)
-}
+         `{ id }`,)
+
+    const countSelectionSet = `
+         {
+             aggregate {
+                 count
+             }
+         }
+         `
+    const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
+
+    return {
+             count: linksConnection.aggregate.count,
+             linkIds: queriedLinks.map(link => link.id)
+            }
+    }
 
 module.exports = {
     feed,
